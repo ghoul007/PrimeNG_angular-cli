03edf93d9e92560063cf4429ac09ce1281765035
Ahmed <ghoul.ahmed5@gmail.com>
2017-12-03 23:09:59 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
+import {MenubarModule,MenuItem} from 'primeng/primeng';
+import {DataTableModule,SharedModule} from 'primeng/primeng';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  cars = [];
+  items: MenuItem[];
+
+      ngOnInit() {
+        this.cars = [{"name":"ahmed","age":28},{"name":"ismail","age":30}]
+          this.items = [
+              {
+                  label: 'File',
+                  items: [{
+                          label: 'New',
+                          icon: 'fa-plus',
+                          items: [
+                              {label: 'Project'},
+                              {label: 'Other'},
+                          ]
+                      },
+                      {label: 'Open'},
+                      {label: 'Quit'}
+                  ]
+              },
+              {
+                  label: 'Edit',
+                  icon: 'fa-edit',
+                  items: [
+                      {label: 'Undo', icon: 'fa-mail-forward'},
+                      {label: 'Redo', icon: 'fa-mail-reply'}
+                  ]
+              }
+          ];
+      }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import {ButtonModule} from 'primeng/primeng';
+import {MenubarModule } from 'primeng/primeng';
+import {DataTableModule,SharedModule} from 'primeng/primeng';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserAnimationsModule, ButtonModule, MenubarModule,DataTableModule,SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
